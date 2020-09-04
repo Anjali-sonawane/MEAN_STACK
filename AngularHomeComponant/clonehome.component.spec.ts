890461c7b542930a6820d33432d4c88acacca405
Anjali-sonawane <64551835+Anjali-sonawane@users.noreply.github.com>
2020-09-04 20:47:03 +0530

    Angular Routing Practice

---
+++ AngularHomeComponant/clonehome.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClonehomeComponent } from './clonehome.component';
+
+describe('ClonehomeComponent', () => {
+  let component: ClonehomeComponent;
+  let fixture: ComponentFixture<ClonehomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ClonehomeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClonehomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ AngularHomeComponant/clonehome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-clonehome',
+  templateUrl: './clonehome.component.html',
+  styleUrls: ['./clonehome.component.css']
+})
+export class ClonehomeComponent implements OnInit {
+  public title = 'CloneHomePage';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
